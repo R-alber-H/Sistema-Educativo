07c52d0a9cc2912dc817fbfa8a479fe9dda44675
Ricardo Alberco <sabado17_08_2024@outlook.com>
2025-12-04 16:05:54 -0500

    agregando layout

--- src/app/layout/layout/layout.ts
+++ src/app/layout/layout/layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from "../../components/navbar/navbar";
-import { RouterOutlet } from '@angular/router'; 
+import { RouterOutlet } from '@angular/router';
+import { Sidebar } from "../../components/sidebar/sidebar"; 
 
 @Component({
   selector: 'app-layout',
-  imports: [Navbar,RouterOutlet],
+  imports: [Navbar, RouterOutlet, Sidebar],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
