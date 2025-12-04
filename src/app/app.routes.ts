e92a95b109d45a561455e41ba3a8418281065c22
Ricardo Alberco <sabado17_08_2024@outlook.com>
2025-12-04 00:09:40 -0500

    agregando login y layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,15 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Layout } from './layout/layout/layout';
 
 export const routes: Routes = [
-{ path: '', component:Login } 
+  { path: '', component: Login },
+  {
+    path: '',
+    component: Layout, 
+    children: [
+      { path: 'dashboard', component: Dashboard },
+    ]
+  }
 ];
