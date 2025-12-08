888b055121dddaf8edfa497d3fdc6c0b0e9501f3
Ricardo Alberco <sabado17_08_2024@outlook.com>
2025-12-08 15:35:19 -0500

    Creación del modal registrar profesor

---
+++ src/app/components/modal-registrar-profesor/modal-registrar-profesor.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-registrar-profesor',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-registrar-profesor.html',
+  styleUrl: './modal-registrar-profesor.css',
+})
+export class ModalRegistrarProfesor {
+
+}
